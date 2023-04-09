b5fc453fb16c3804b79ad8d8e822fa6ada2f05d2
louispaulet <louispaulet13@gmail.com>
2023-04-10 00:01:22 +0200

    add menus

--- menuData.js
+++ menuData.js
@@ -49,7 +49,152 @@ const menuData = [
         ],
         "grand_total": 250,
         "dining_room_description": "L'Etoile d'Or offers a luxurious and intimate dining experience, adorned with opulent gold accents, crystal chandeliers, and elegant silk drapery. The soft lighting and plush velvet seating create an atmosphere of romance and sophistication, while the floor-to-ceiling windows offer breathtaking views of the Eiffel Tower."
+    },
+{
+  "location": "Four Seasons Hotel, Kyoto",
+  "restaurant_name": "Sakura No Hana",
+  "chef_name": "Chef Katsuro Tanaka",
+  "tasting_menu": [
+    {
+      "course": "Sakizuke",
+      "description": "Seasonal appetizer with Kyoto vegetables, tofu, and yuzu miso",
+      "price": 20,
+      "wine_pairing": "Dassai 23 Junmai Daiginjo Sake"
+    },
+    {
+      "course": "Sashimi",
+      "description": "Assorted fresh sashimi selection served with house-made soy sauce and wasabi",
+      "price": 45,
+      "wine_pairing": "Kubota Manju Junmai Daiginjo Sake"
+    },
+    {
+      "course": "Nimono",
+      "description": "Simmered seasonal fish with daikon radish, ginger, and Kyoto miso",
+      "price": 40,
+      "wine_pairing": "Hakutsuru Excellent Junmai Sake"
+    },
+    {
+      "course": "Tempura",
+      "description": "Crispy tempura featuring seasonal vegetables and shrimp, served with matcha salt and tentsuyu sauce",
+      "price": 35,
+      "wine_pairing": "Tatenokawa 18 Junmai Daiginjo Sake"
+    },
+    {
+      "course": "Yakimono",
+      "description": "Grilled Wagyu beef with shiitake mushrooms, asparagus, and a yuzu-kosho sauce",
+      "price": 65,
+      "wine_pairing": "Isojiman Junmai Daiginjo Sake"
+    },
+    {
+      "course": "Gohan",
+      "description": "Koshihikari rice cooked with seasonal ingredients, miso soup, and pickles",
+      "price": 30,
+      "wine_pairing": "Hakkaisan Junmai Ginjo Sake"
+    },
+    {
+      "course": "Dessert",
+      "description": "Matcha mousse with azuki bean paste and yuzu sorbet",
+      "price": 20,
+      "wine_pairing": "Umetsu Yuzushu"
     }
-    // Add more menus to the array if needed
+  ],
+  "grand_total": 255,
+  "dining_room_description": "Sakura No Hana provides an exquisite and serene dining experience, featuring traditional Japanese architecture and design elements. Guests are surrounded by natural wood and stone, with delicate cherry blossom motifs adorning the walls. The floor-to-ceiling windows overlook a beautiful Japanese garden, creating an atmosphere of tranquility and harmony."
+},
+
+{
+  "location": "Four Seasons Resort, Bali at Jimbaran Bay",
+  "restaurant_name": "Taman Sari",
+  "chef_name": "Chef Made Agus",
+  "tasting_menu": [
+    {
+      "course": "Amuse-Bouche",
+      "description": "Balinese spiced tuna tartare on crispy cassava chip",
+      "price": 15,
+      "wine_pairing": "Cloudy Bay Sauvignon Blanc 2020"
+    },
+    {
+      "course": "Appetizer",
+      "description": "Gado-gado salad with peanut sauce and fresh local vegetables",
+      "price": 25,
+      "wine_pairing": "Domaine Laroche Chablis Saint Martin 2019"
+    },
+    {
+      "course": "Soup",
+      "description": "Soto Ayam, a traditional Indonesian chicken soup with lemongrass, turmeric, and glass noodles",
+      "price": 20,
+      "wine_pairing": "Dr. Loosen Blue Slate Riesling Kabinett 2019"
+    },
+    {
+      "course": "Fish",
+      "description": "Ikan Bakar, grilled Jimbaran Bay fish with sambal matah and steamed rice",
+      "price": 45,
+      "wine_pairing": "Pascal Jolivet Pouilly-Fumé 2020"
+    },
+    {
+      "course": "Meat",
+      "description": "Babi Guling, Balinese roast suckling pig with urap vegetables and lawar salad",
+      "price": 55,
+      "wine_pairing": "Penfolds Bin 28 Kalimna Shiraz 2018"
+    },
+    {
+      "course": "Dessert",
+      "description": "Dadar Gulung, coconut crepes with palm sugar syrup and vanilla ice cream",
+      "price": 20,
+      "wine_pairing": "Château Rieussec Sauternes 2017"
+    }
+  ],
+  "grand_total": 180,
+  "dining_room_description": "Taman Sari, meaning 'Water Garden' in Indonesian, offers a breathtaking open-air dining experience. The restaurant is adorned with traditional Balinese carvings, lush tropical greenery, and water features. Guests can enjoy the stunning ocean views while indulging in the flavors of Bali, as the gentle sea breeze adds to the enchanting atmosphere."
+},
+
+{
+  "location": "Four Seasons Resort, Bora Bora",
+  "restaurant_name": "Le Lagon",
+  "chef_name": "Chef Matthieu Guérin",
+  "tasting_menu": [
+    {
+      "course": "Amuse-Bouche",
+      "description": "Tahitian-style poisson cru with coconut milk and lime",
+      "price": 20,
+      "wine_pairing": "Château d'Esclans Whispering Angel Rosé 2020"
+    },
+    {
+      "course": "Appetizer",
+      "description": "Grilled octopus with mango salsa and vanilla-infused olive oil",
+      "price": 35,
+      "wine_pairing": "Dog Point Vineyard Sauvignon Blanc 2020"
+    },
+    {
+      "course": "Soup",
+      "description": "Chilled cucumber and pineapple soup with mint and rum-infused yogurt",
+      "price": 20,
+      "wine_pairing": "Robert Mondavi Fumé Blanc 2019"
+    },
+    {
+      "course": "Fish",
+      "description": "Pan-seared mahi-mahi with sweet potato purée, sautéed bok choy, and passionfruit beurre blanc",
+      "price": 50,
+      "wine_pairing": "Domaine Louis Moreau Chablis 2019"
+    },
+    {
+      "course": "Meat",
+      "description": "Grilled New Zealand lamb chops with taro root gratin, broccolini, and rosemary jus",
+      "price": 60,
+      "wine_pairing": "Craggy Range Te Kahu Gimblett Gravels Vineyard 2018"
+    },
+    {
+      "course": "Dessert",
+      "description": "Vanilla bean panna cotta with poached papaya and coconut tuile",
+      "price": 20,
+      "wine_pairing": "Royal Tokaji Late Harvest 2017"
+    }
+  ],
+  "grand_total": 205,
+  "dining_room_description": "Le Lagon offers an unparalleled tropical dining experience, with a stunning open-air setting overlooking the turquoise waters of Bora Bora's lagoon. Surrounded by lush vegetation, the restaurant features Polynesian-inspired design elements, such as thatched-roof pavilions, hand-carved sculptures, and warm lighting. Guests can savor the exquisite cuisine while enjoying the gentle ocean breeze and breathtaking views of Mount Otemanu."
+}
+
+
+
 ];
 
